c3de3cc9a2a27fd5353f13eb1001c1547dea685e
Joaquin Sedoff <joacelrusito@gmail.com>
2025-03-20 15:14:04 -0300

    cambio de tiempo

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ sleep(1)    #le damos tiempo a vREPL
 print("\nLED esta destellando...")
 while True:
     try:
-        led_board.toggle()
-        # led_board.value(not led_board.value())
-        sleep(.5) # sleep 1sec
+        led_board.toggle() 
+        # led_board.value(not led_board.value()) #para esp32, lee el estado, lo niega y pasa como argumento 
+        sleep(.25) # sleep 0.5sec
     except KeyboardInterrupt:
         break
 led_board.off()
